dc8f9c5419536a9efbebaa38db7b99cef612a65b
mrmani2002 <mrmani17mkr@gmail.com>
2025-08-13 11:48:15 +0530

    done few codes

---
+++ src/org/git/GITDay1.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class GITDay1 {
+	
+	private void empId() {
+		System.out.println("Employee id is available");
+	}
+	private void empName() {
+		System.out.println("Employee name is available");
+	}
+	
+
+}
